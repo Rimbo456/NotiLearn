ee2655a5abd0e8afce8839f61e4e44145ee11962
DuyThu <duythutiktok@gmail.com>
2026-06-05 15:21:46 +0700

    feat: update HttpTimeout interceptor

--- shared/core/network/src/commonMain/kotlin/com/rim/notilearn/core/network/client/HttpClientConfig.kt
+++ shared/core/network/src/commonMain/kotlin/com/rim/notilearn/core/network/client/HttpClientConfig.kt
@@ -2,6 +2,7 @@ package com.rim.notilearn.core.network.client
 
 import com.rim.notilearn.core.network.serialization.JsonSerializer
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
@@ -11,6 +12,12 @@ import io.ktor.serialization.kotlinx.json.json
 object HttpClientConfig {
     fun configure(client: HttpClient) {
         client.config {
+            install(HttpTimeout) {
+                requestTimeoutMillis = 15000L
+                connectTimeoutMillis = 15000L
+                socketTimeoutMillis = 15000L
+            }
+
             install(Logging) {
                 level = LogLevel.ALL
                 logger = object : Logger {
