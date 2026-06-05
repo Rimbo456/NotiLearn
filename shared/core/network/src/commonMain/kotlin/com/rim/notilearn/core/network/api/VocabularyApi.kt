961569e4ea6ac64270fcf1cd4e46b4278448f20e
DuyThu <duythutiktok@gmail.com>
2026-06-05 17:50:06 +0700

    feat: add error handling in Vocabulary Api

--- shared/core/network/src/commonMain/kotlin/com/rim/notilearn/core/network/api/VocabularyApi.kt
+++ shared/core/network/src/commonMain/kotlin/com/rim/notilearn/core/network/api/VocabularyApi.kt
@@ -10,34 +10,39 @@ import io.ktor.client.request.url
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.http.path
+import kotlinx.io.IOException
 
 class VocabularyApi(
     private val httpClient: HttpClient
 ) {
     suspend fun generateVocabularyContent(rawPrompt: String): String {
-        val requestBody = mapOf(
-            "content" to listOf(
-                mapOf(
-                    "parts" to listOf(
-                        mapOf("text" to rawPrompt)
+        try {
+            val requestBody = mapOf(
+                "content" to listOf(
+                    mapOf(
+                        "parts" to listOf(
+                            mapOf("text" to rawPrompt)
+                        )
                     )
+                ),
+                "generationConfig" to mapOf(
+                    "responseMimeType" to "application/json"
                 )
-            ),
-            "generationConfig" to mapOf(
-                "responseMimeType" to "application/json"
             )
-        )
 
-        val response: GeminiResponse = httpClient.post {
-            url("https://generativelanguage.googleapis.com")
-            url.path("v1beta", "models", "gemini-3.5-flash:generateContent")
-            url.parameters.append("key", BuildKonfig.GEMINI_API_KEY)
-            contentType(ContentType.Application.Json)
-            setBody(requestBody)
-        }.body()
+            val response: GeminiResponse = httpClient.post {
+                url("https://generativelanguage.googleapis.com")
+                url.path("v1beta", "models", "gemini-3.5-flash:generateContent")
+                url.parameters.append("key", BuildKonfig.GEMINI_API_KEY)
+                contentType(ContentType.Application.Json)
+                setBody(requestBody)
+            }.body()
 
-        val text = response.candidates.firstOrNull()?.content?.parts?.firstOrNull()?.text
-            ?: ""
-        return text
+            val text = response.candidates.firstOrNull()?.content?.parts?.firstOrNull()?.text
+                ?: ""
+            return text
+        } catch (e: IOException) {
+            throw e
+        }
     }
 }
